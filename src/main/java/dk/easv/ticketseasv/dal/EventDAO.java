d1880b27f8571030d536c20cf9e4f8883cfc68af
ConMcg2025 <cjmcgrail2011@hotmail.com>
2026-03-30 16:44:53 +0200

    Created addevent and editevent methods
    Planning to setup the db before i finish them

--- src/main/java/dk/easv/ticketseasv/dal/EventDAO.java
+++ src/main/java/dk/easv/ticketseasv/dal/EventDAO.java
@@ -1,6 +1,39 @@
 package dk.easv.ticketseasv.dal;
 
+import dk.easv.ticketseasv.be.Event;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class EventDAO
 {
+    ConnectionManager conMan = new ConnectionManager();
 
+    public int addEvent(String name, String description, LocalTime start, LocalTime end, LocalDate Date, String location)
+    {
+        try(Connection con = conMan.getConnection())
+        {
+
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error adding event: " + e.getMessage());
+            return 0;
+        }
+        return 0;
+    }
+
+    public void editEvent(Event event)
+    {
+        try(Connection con = conMan.getConnection())
+        {
+
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error editing event: " + e.getMessage());
+        }
+    }
 }
